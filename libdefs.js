66d9e44811fde18deafd2e3d57bd7c0dc8b85412
Wendell <wendell@Wendells-Mac-mini.local>
2019-09-04 16:58:40 +0800

    add flow configuration

---
+++ libdefs.js
@@ -0,0 +1,72 @@
+declare module 'react-native' {
+  declare module.exports: any;
+}
+declare module 'core-js' {
+  declare module.exports: any;
+}
+declare module 'react-devtools' {
+  declare module.exports: any;
+}
+declare module 'react-native-console-time-polyfill' {
+  declare module.exports: any;
+}
+declare module 'moment' {
+  declare module.exports: any;
+}
+declare module 'redux-saga/effects' {
+  declare module.exports: any;
+}
+declare module 'validator' {
+  declare module.exports: any;
+}
+declare module 'react-native-extended-stylesheet' {
+  declare module.exports: any;
+}
+declare module 'react-navigation' {
+  declare module.exports: any;
+}
+declare module 'axios' {
+  declare module.exports: any;
+}
+declare module 'redux' {
+  declare module.exports: any;
+}
+declare module 'firebase/app' {
+  declare module.exports: any;
+}
+declare module 'firebase/auth' {
+  declare module.exports: any;
+}
+declare module 'firebase/firestore' {
+  declare module.exports: any;
+}
+declare module 'firebase/storage' {
+  declare module.exports: any;
+}
+declare module 'firebase/messaging' {
+  declare module.exports: any;
+}
+declare module 'react-redux' {
+  declare module.exports: any;
+}
+declare module 'redux-logger' {
+  declare module.exports: any;
+}
+declare module 'redux-saga' {
+  declare module.exports: any;
+}
+declare module 'redux-persist' {
+  declare module.exports: any;
+}
+declare module 'redux-persist/es/integration/react' {
+  declare module.exports: any;
+}
+declare module 'react-native-config' {
+  declare module.exports: any;
+}
+declare module 'prop-types' {
+  declare module.exports: any;
+}
+declare module 'color' {
+  declare module.exports: any;
+}
