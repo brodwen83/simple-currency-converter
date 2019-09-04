8dcf6b9f418d097fcb20b448a5d73908a222027a
Wendell <wendell@Wendells-Mac-mini.local>
2019-09-04 16:35:47 +0800

    animate logo when keyboard shows and hides

--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,16 +4,28 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const imageWidth = Dimensions.get('window').width / 2;
 
 export default EStyleSheet.create({
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
+
   container: {
     alignItems: 'center',
   },
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
+    width: '$largeContainerSize',
+    height: '$largeContainerSize',
   },
-  image: {
-    width: imageWidth / 3,
+  backgroundImage: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    flex: 1,
+  },
+  logo: {
+    width: '$largeImageSize',
   },
   text: {
     fontWeight: '600',
